ceb62e2bfbcd339d0c8b9cfa41026ca560e67c9c
Kairarara <45175730+Kairarara@users.noreply.github.com>
2019-06-13 19:56:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+let helmet      = require('helmet')
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+//Only allow your site to be loading in an iFrame on your own pages.
+app.use(helmet.frameguard({action: 'sameorigin' }));
+//Do not allow DNS prefetching.
+app.use(helmet.dnsPrefetchControl({allow: false}));
+//Only allow your site to send the referrer for your own pages
+app.use(helmet.referrerPolicy({policy:'same-origin'}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -48,6 +56,27 @@ app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
+});
+
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
+
 });
 
 //Start our server and tests!
